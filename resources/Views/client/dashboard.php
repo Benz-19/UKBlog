981a2e3dede4f16a91b7cc51ee74946ba11ed44f
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-06-08 15:08:58 +0300

    update

--- resources/Views/client/dashboard.php
+++ resources/Views/client/dashboard.php
@@ -4,11 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/assets/css/styles.css">
     <title>Client Dashboard</title>
 </head>
 
 <body>
+    <!-- Header -->
+    <?php require __DIR__ . '/../layouts/header.php'; ?>
+
+    <!-- Content -->
     <h1>Welcome Client</h1>
+
+    <!-- Footer -->
+    <?php require __DIR__ . '/../layouts/footer.php'; ?>
 </body>
 
 </html>
